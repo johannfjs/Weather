65851ac1aa8d9cf5d29f623b1ebf85c030636976
Johann <johannfjs@MacBook-Pro-de-Johann.local>
2016-10-08 17:11:21 -0500

    first commit

---
+++ app/src/main/java/com/jcodee/fragments/WeatherFragment.java
@@ -0,0 +1,157 @@
+package com.jcodee.fragments;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.jcodee.preferences.CityPreference;
+import com.jcodee.utils.RemoteFetch;
+import com.jcodee.weather.R;
+
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by johannfjs on 8/10/16.
+ */
+
+public class WeatherFragment extends Fragment {
+    @BindView(R.id.city_field)
+    TextView cityField;
+    @BindView(R.id.updated_field)
+    TextView updatedField;
+    @BindView(R.id.weather_icon)
+    TextView weatherIcon;
+    @BindView(R.id.current_temperature_field)
+    TextView currentTemperatureField;
+    @BindView(R.id.details_field)
+    TextView detailsField;
+    private Typeface weatherFont;
+    private Handler handler;
+
+    public WeatherFragment() {
+        handler = new Handler();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        weatherFont = Typeface.createFromAsset(getActivity().getAssets(), "fonts/weather.ttf");
+        updateWeatherData(new CityPreference(getActivity()).getCity());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_weather, container, false);
+        ButterKnife.bind(this, root);
+
+        weatherIcon.setTypeface(weatherFont);
+        return root;
+    }
+
+    private void updateWeatherData(final String city) {
+        new Thread() {
+            public void run() {
+                final JSONObject json = RemoteFetch.getJSON(getActivity(), city);
+                if (json == null) {
+                    handler.post(new Runnable() {
+                        public void run() {
+                            Toast.makeText(getActivity(),
+                                    getActivity().getString(R.string.place_not_found),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+                } else {
+                    handler.post(new Runnable() {
+                        public void run() {
+                            renderWeather(json);
+                        }
+                    });
+                }
+            }
+        }.start();
+    }
+
+    private void renderWeather(JSONObject json) {
+        try {
+            cityField.setText(json.getString("name").toUpperCase(Locale.US) +
+                    ", " +
+                    json.getJSONObject("sys").getString("country"));
+
+            JSONObject details = json.getJSONArray("weather").getJSONObject(0);
+            JSONObject main = json.getJSONObject("main");
+            detailsField.setText(
+                    details.getString("description").toUpperCase(Locale.US) +
+                            "\n" + "Humidity: " + main.getString("humidity") + "%" +
+                            "\n" + "Pressure: " + main.getString("pressure") + " hPa");
+
+            currentTemperatureField.setText(
+                    String.format("%.2f", main.getDouble("temp")) + " ℃");
+
+            DateFormat df = DateFormat.getDateTimeInstance();
+            String updatedOn = df.format(new Date(json.getLong("dt") * 1000));
+            updatedField.setText("Last update: " + updatedOn);
+
+            setWeatherIcon(details.getInt("id"),
+                    json.getJSONObject("sys").getLong("sunrise") * 1000,
+                    json.getJSONObject("sys").getLong("sunset") * 1000);
+
+        } catch (Exception e) {
+            Log.e("SimpleWeather", "One or more fields not found in the JSON data");
+        }
+    }
+
+    private void setWeatherIcon(int actualId, long sunrise, long sunset) {
+        int id = actualId / 100;
+        String icon = "";
+        if (actualId == 800) {
+            long currentTime = new Date().getTime();
+            if (currentTime >= sunrise && currentTime < sunset) {
+                icon = getActivity().getString(R.string.weather_sunny);
+            } else {
+                icon = getActivity().getString(R.string.weather_clear_night);
+            }
+        } else {
+            switch (id) {
+                case 2:
+                    icon = getActivity().getString(R.string.weather_thunder);
+                    break;
+                case 3:
+                    icon = getActivity().getString(R.string.weather_drizzle);
+                    break;
+                case 7:
+                    icon = getActivity().getString(R.string.weather_foggy);
+                    break;
+                case 8:
+                    icon = getActivity().getString(R.string.weather_cloudy);
+                    break;
+                case 6:
+                    icon = getActivity().getString(R.string.weather_snowy);
+                    break;
+                case 5:
+                    icon = getActivity().getString(R.string.weather_rainy);
+                    break;
+            }
+        }
+        weatherIcon.setText(icon);
+    }
+
+    public void changeCity(String city) {
+        updateWeatherData(city);
+    }
+}
---
+++ app/src/main/java/com/jcodee/preferences/CityPreference.java
@@ -0,0 +1,24 @@
+package com.jcodee.preferences;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+
+/**
+ * Created by johannfjs on 8/10/16.
+ */
+
+public class CityPreference {
+    SharedPreferences prefs;
+
+    public CityPreference(Activity activity) {
+        prefs = activity.getPreferences(Activity.MODE_PRIVATE);
+    }
+
+    public String getCity() {
+        return prefs.getString("city", "Sydney, AU");
+    }
+
+    public void setCity(String city) {
+        prefs.edit().putString("city", city).commit();
+    }
+}
